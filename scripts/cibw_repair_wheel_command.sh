328cf7d10a5c4a6c49c11ae6bd6415e7b5fbff80
Sergey B Kirpichev <skirpichev@gmail.com>
2024-12-21 14:58:23 +0300

    +1

--- scripts/cibw_repair_wheel_command.sh
+++ scripts/cibw_repair_wheel_command.sh
@@ -4,6 +4,8 @@ DEST_DIR=$1
 WHEEL=$2
 LD_LIBRARY_PATH="$(pwd)/.local/lib:$LD_LIBRARY_PATH"
 
+unzip -l ${WHEEL}
+
 if [[ "$OSTYPE" == "darwin"* ]]
 then
   delocate-wheel --lib-sdir ../python-gmp.libs -w ${DEST_DIR} -v ${WHEEL}
