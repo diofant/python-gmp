de2c7172ef71427b22077fdc0ad437533e8b25cd
Sergey B Kirpichev <skirpichev@gmail.com>
2025-08-15 13:59:44 +0300

    Drop workarounds for PyPy3.11
    Closes #233

--- tests/test_mpz.py
+++ tests/test_mpz.py
@@ -101,9 +101,8 @@ def test___format___interface():
     mx = mpz(123)
     with pytest.raises(ValueError, match="Unknown format code"):
         format(mx, "q")
-    if platform.python_implementation() != "PyPy":  # XXX
-        with pytest.raises(ValueError, match="Unknown format code"):
-            format(mx, "\x81")
+    with pytest.raises(ValueError, match="Unknown format code"):
+        format(mx, "\x81")
     with pytest.raises(ValueError,
                        match=(r"Negative zero coercion \(z\) not allowed|"
                               "Invalid conversion specification")):
@@ -168,10 +167,6 @@ def test___format___interface():
     if sys.version_info >= (3, 14):
         assert format(mx, ".,f") == "123.000,000"
         assert format(mx, "._f") == "123.000_000"
-
-    if (platform.python_implementation() == "PyPy"
-            and sys.pypy_version_info[:3] <= (7, 3, 20)):
-        return  # issue pypy/pypy#5311
 
     try:
         locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
@@ -660,11 +655,11 @@ def test_power_mod(x, y, z):
         assert pow(mx, my, mz) == r
         assert pow(mx, my, z) == r
         assert pow(mx, y, mz) == r
-        if platform.python_implementation() == "PyPy":
-            return  # XXX: pypy/pypy#5207
         assert pow(x, my, mz) == r
         assert pow(mx, y, z) == r
         assert pow(x, my, z) == r
+        if platform.python_implementation() == "PyPy":
+            return  # XXX: pypy/pypy#5207
         assert pow(x, y, mz) == r
 
 
