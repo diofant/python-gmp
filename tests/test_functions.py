7e6585cc9012d3ec4c2ab88a37e10e5d95c0c4c1
Sergey B Kirpichev <skirpichev@gmail.com>
2025-08-30 10:11:19 +0300

    Add API tests
    Closes #253

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,3 +1,4 @@
+import inspect
 import math
 
 import gmp
@@ -236,3 +237,10 @@ def test_interfaces():
     with pytest.raises(ValueError, match="invalid rounding mode specified"):
         _mpmath_normalize(1, mpz(111), 11, 12, 13, 1j)
     gmp._free_cache()  # just for coverage
+
+
+def test_func_api():
+    for fn in ["comb", "factorial", "gcd", "isqrt", "lcm", "perm"]:
+        f = getattr(math, fn)
+        fz = getattr(gmp, fn)
+        assert inspect.signature(f) == inspect.signature(fz)
