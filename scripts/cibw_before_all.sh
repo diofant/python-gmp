fba10a78ad37987c23e2ecdca395781d38bf682a
Sergey B Kirpichev <skirpichev@gmail.com>
2026-02-13 13:29:16 +0300

    Use libzz v0.9.0a2

--- scripts/cibw_before_all.sh
+++ scripts/cibw_before_all.sh
@@ -53,7 +53,7 @@ make --silent all install
 
 cd ..
 
-ZZ_VERSION=0.8.0
+ZZ_VERSION=0.9.0a2
 ZZ_DIR=zz-${ZZ_VERSION}
 ZZ_URL=https://github.com/diofant/zz/releases/download/v${ZZ_VERSION}/${ZZ_DIR}.tar.gz
 
