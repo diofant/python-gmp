f9ae7b350d8c3de644fc1d003cc3914cfdc4f178
Sergey B Kirpichev <skirpichev@gmail.com>
2026-02-19 14:16:57 +0300

    Add collatz2() in bench/collatz.py
    (cherry picked from commit 6663edfe08249af081dcec7407395f44c79ad97d)
    
    # Conflicts:
    #	bench/collatz.py

---
+++ bench/collatz.py
@@ -0,0 +1,53 @@
+# collatz.py
+
+import os
+
+import pyperf
+
+if os.getenv("T") == "gmpy2.mpz":
+    from gmpy2 import mpz
+elif os.getenv("T") == "flint.fmpz":
+    from flint import fmpz as mpz
+elif os.getenv("T") == "int":
+    mpz = int
+else:
+    from gmp import mpz
+
+zero = mpz(0)
+one = mpz(1)
+two = mpz(2)
+three = mpz(3)
+
+# https://en.wikipedia.org/wiki/Collatz_conjecture
+
+def collatz0(n):
+    total = 0
+    n = mpz(n)
+    while n > one:
+        n = n*three + one if n & one else n//two
+        total += 1
+    return total
+
+def collatz1(n):
+    total = 0
+    n = mpz(n)
+    while n > 1:
+        n = n*3 + 1 if n & 1 else n//2
+        total += 1
+    return total
+
+def collatz2(n):
+    total = 0
+    n = mpz(n)
+    while n > 1:
+        n = n*3 + 1 if n & one else n//2
+        total += 1
+    return total
+
+
+runner = pyperf.Runner()
+for f in [collatz0, collatz1, collatz2]:
+    for v in ["97", "871", "(1<<128)+31"]:
+        h = f"{f.__name__}({v})"
+        i = eval(v)
+        runner.bench_func(h, f, i)
