c424da0ea53cfd4a597c2e92fe002be818204ad2
Sergey B Kirpichev <skirpichev@gmail.com>
2026-02-20 07:07:46 +0300

    Use libzz v0.9.0a4
    (cherry picked from commit 33cfa0e54b9e1657a8da4bf8dbfd962094252933)
    
    # Conflicts:
    #	.readthedocs.yaml
    #	scripts/cibw_before_all.sh

--- scripts/cibw_before_all.sh
+++ scripts/cibw_before_all.sh
@@ -22,8 +22,32 @@ unset CFLAGS
 # We replace config.guess with configfsf.guess to avoid microarchitecture
 # specific code in common code.
 rm config.guess && mv configfsf.guess config.guess && chmod +x config.guess
+<<<<<<< HEAD
 ./configure --enable-fat \
             --enable-shared \
+=======
+
+./configure ${CONFIG_ARGS}
+
+make --silent all install
+
+cd ..
+
+ZZ_VERSION=0.9.0a4
+ZZ_DIR=zz-${ZZ_VERSION}
+ZZ_URL=https://github.com/diofant/zz/releases/download/v${ZZ_VERSION}/${ZZ_DIR}.tar.gz
+
+download ${ZZ_URL}
+tar --extract --file ${ZZ_DIR}.tar.gz
+cd ${ZZ_DIR}
+
+if [ "$OSTYPE" = "cygwin" ] && [ "${RUNNER_ARCH}" = "ARM64" ]
+then
+  autoreconf -if
+fi
+
+./configure --enable-shared \
+>>>>>>> 33cfa0e (Use libzz v0.9.0a4)
             --disable-static \
             --with-pic \
             --disable-alloca \
